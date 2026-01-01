938a8401613e43ce55cab0ae5f59c7323c30558f
Amandeep <amandeep@groupgnx.org>
2026-01-01 12:34:34 +0530

    Remove commented-out image code in FaqCard component to clean up the codebase and maintain focus on icon usage for visual consistency.

--- src/components/main/FaqCard.tsx
+++ src/components/main/FaqCard.tsx
@@ -1,6 +1,4 @@
 "use client"
-
-// import { ClassNames } from "@emotion/react";
 import React from "react";
 
 const FaqCard = ({ title, icon, fc, className}:
@@ -8,7 +6,6 @@ const FaqCard = ({ title, icon, fc, className}:
     return (
       <button onClick={()=> fc()} className={`flex items-center border border-gray-200 w-full lg:max-w-[260px] rounded-md min-h-[120px] bg-white gap-6 shadow-md  ${ className}`}>
         <div className="w-full flex justify-center gap-2 items-center">
-          {/* <img src={img} alt={imgTitle} className="w-16 h-16"/> */}
           <span className="text-3xl">{icon}</span>
           <h4 className="text-[#4b4d4c] text-xl font-bold font-arial">{title}</h4>
         </div>
