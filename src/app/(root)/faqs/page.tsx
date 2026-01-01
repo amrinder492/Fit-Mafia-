04354dc5ed5fa2ddacf47e05a34b019f865c898a
Amandeep <amandeep@groupgnx.org>
2026-01-01 12:41:52 +0530

    Enhance FAQ layout by adjusting flex properties for improved responsiveness and alignment across different screen sizes.

--- src/app/(root)/faqs/page.tsx
+++ src/app/(root)/faqs/page.tsx
@@ -29,7 +29,7 @@ const Page = () => {
         />
       </div> */}
       {/* questions */}
-      <div className="flex items-center justify-center gap-5 w-full h-full  xl:px-12 py-2 ">
+      <div className="flex flex-col lg:flex-row items-center justify-center gap-5 w-full h-full px-4 lg:px-0 xl:px-12 py-2 ">
         {faqs.map((data) => {
           const isActive = selectedFaqGroup === data.value;
           return (
